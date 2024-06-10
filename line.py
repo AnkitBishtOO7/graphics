64fb4f47f6c5686a243b07ee5e35e45e57b61b80
AnkitBishtOO7 <ankit2004alm@gmail.com>
2024-06-10 19:38:15 +0530

    Update line.py

--- line.py
+++ line.py
@@ -1,60 +1,20 @@
 import pygame
-import sys
 
-def bresenham_line(screen, x1, y1, x2, y2, color):
-    dx = abs(x2 - x1)
-    dy = abs(y2 - y1)
-    sx = 1 if x1 < x2 else -1
-    sy = 1 if y1 < y2 else -1
+def bresenham_line(screen, x0, y0, x1, y1, color):
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y0 < y1 else -1
     err = dx - dy
 
     while True:
-        screen.set_at((x1, y1), color)
-        if x1 == x2 and y1 == y2:
+        screen.set_at((x0, y0), color)
+        if x0 == x1 and y0 == y1:
             break
         e2 = err * 2
         if e2 > -dy:
             err -= dy
-            x1 += sx
+            x0 += sx
         if e2 < dx:
             err += dx
-            y1 += sy
-
-def main():
-    # line parameters
-    x1 = int(input("Enter the x-coordinate of the start point: "))
-    y1 = int(input("Enter the y-coordinate of the start point: "))
-    x2 = int(input("Enter the x-coordinate of the end point: "))
-    y2 = int(input("Enter the y-coordinate of the end point: "))
-    print("switch to pygame terminal to see results")
-
-    # Initialize Pygame
-    pygame.init()
-
-    # Screen dimensions
-    width, height = 800, 600
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Bresenham\'s Line Algorithm')
-
-    # Colors
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-
-    running = True
-    screen.fill(WHITE)
-    
-    bresenham_line(screen, x1, y1, x2, y2, BLACK)
-    
-    pygame.display.flip()
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-    pygame.quit()
-    sys.exit()
-
-if __name__ == "__main__":
-    main()
-
+            y0 += sy
